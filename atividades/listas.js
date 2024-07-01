166617845b3ec2a29be228e344592c3d41a696af
Flavia-20 <flaviamonique38@gmail.com>
2024-07-01 15:42:26 -0300

    Atividade com listas

---
+++ atividades/listas.js
@@ -0,0 +1,24 @@
+listaVazia = [];
+
+linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin','Python'];
+console.log(linguagensDeProgramacao);
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+console.log(linguagensDeProgramacao);
+
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+console.log(linguagensDeProgramacao);
+
+listaNomes = ['Ana','Marcos','Andre'];
+console.log(listaNomes[0]);
+console.log(listaNomes[1]);
+console.log(listaNomes[2]);
+
+
+
+
+
+
