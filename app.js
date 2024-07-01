5e1489ee37997bdfd15f8ebd38cd63756008f18b
Flavia-20 <flaviamonique38@gmail.com>
2024-06-30 23:56:50 -0300

    reiniciando o jogo

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 
 let numeroSecreto = gerarNumeroAleatorio();
-
+let tentativas = 1;
 
 function exibirTextoInicialNaTela(tag,texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
+function exibirMensagemInicial(){
+    exibirTextoInicialNaTela('h1','Jogo do numero secreto')// todo lugar qu a função for chamada vai modificar o h1
+    exibirTextoInicialNaTela('p','Escolha um número entre 1 e 10')// todo lugar qu a função for chamada vai modificar o p
+}
+
 //função sem retorno e sem parametro
 function verificarChute(){
     let chute = document.querySelector('input').value;
 
+    if(chute == numeroSecreto){
+        exibirTextoInicialNaTela('h1','Acertou');
+        let palavraTentativas = tentativas < 1? 'tentativas': 'tentativa';
+        let mensagemTentativas = `Voce descobriu o numero secreto com ${tentativas} ${palavraTentativas}!`
+        exibirTextoInicialNaTela('p',mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
 
-    console.log(chute == numeroSecreto);
+    }else{
+        if(chute > numeroSecreto){
+            exibirTextoInicialNaTela('p','O numero secreto é menor');
+        }else{
+            exibirTextoInicialNaTela('p', 'O numero secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+    
 }
 
 //função com retorno e sem parametro
@@ -20,8 +40,16 @@ function gerarNumeroAleatorio() {
    return parseInt( Math.random() * 10 + 1);
 }
 
+function limparCampo() {
+    chute = document.querySelector('input')
+    chute.value = '';
+}
 
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial()
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+}
 
-
-exibirTextoInicialNaTela('h1','Jogo do numero secreto')// todo lugar qu a função for chamada vai modificar o h1
-exibirTextoInicialNaTela('p','Escolha um número entre 1 e 10')// todo lugar qu a função for chamada vai modificar o p
